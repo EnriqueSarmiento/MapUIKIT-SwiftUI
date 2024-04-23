36524bb80b03b180fd5799177a66775c31282e6a
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-04-23 17:49:33 -0400

    FEATURE: Added swift ui view map

---
+++ MapUIKIT&SwiftUI/MapSwiftUI.swift
@@ -0,0 +1,18 @@
+//
+//  MapSwiftUI.swift
+//  MapUIKIT&SwiftUI
+//
+//  Created by Enrique Sarmiento on 23/4/24.
+//
+
+import SwiftUI
+
+struct MapSwiftUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapSwiftUI()
+}
