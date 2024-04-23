b872acbbec83ef92c92eee2c719a7e95601d24f3
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-04-23 16:58:15 -0400

    Initial Commit

---
+++ MapUIKIT&SwiftUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapUIKIT&SwiftUI
+//
+//  Created by Enrique Sarmiento on 23/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
