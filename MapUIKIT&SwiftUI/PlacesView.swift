e0dd562b6c7eb764d80214b83cf77ab42a2757bc
Enrique Sarmiento <jeisone.sarmiento@gmail.com>
2024-04-26 12:31:12 -0400

    FEATURE: added array of places to show pin and locations with swiftUI

---
+++ MapUIKIT&SwiftUI/PlacesView.swift
@@ -0,0 +1,18 @@
+//
+//  PlacesView.swift
+//  MapUIKIT&SwiftUI
+//
+//  Created by Enrique Sarmiento on 26/4/24.
+//
+
+import SwiftUI
+
+struct PlacesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlacesView()
+}
